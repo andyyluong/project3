f2b1e38ec9359e48b3cd80563140eccf2888c679
Brian-Justice <33060685+Brian-Justice@users.noreply.github.com>
2017-11-09 15:58:49 -0500

    Changed brackets, now Serializable

--- src/project3/Inventory.java
+++ src/project3/Inventory.java
@@ -9,60 +9,49 @@ package project3;
  *
  * @author brian
  */
-public class Inventory 
-{
+public class Inventory implements Seriablizable{
     private BikePart bikePart;
     private Boolean onSale;
     private int quantity;
     
-    public Inventory(BikePart bikepart, Boolean onSale)
-    {
+    public Inventory(BikePart bikepart, Boolean onSale){
         this.bikePart = bikePart;
         this.onSale = onSale;
     }
-    public void add(int number)
-    {
+    public void add(int number){
         quantity += number;
     }
     
-    public void take(int number)
-    {
+    public void take(int number){
         quantity -= number;
     }
     
-    public void setOnSale(boolean onSale)
-    {
+    public void setOnSale(boolean onSale){
         this.onSale = onSale;
     }
     
-    public Boolean getOnSale()
-    {
+    public Boolean getOnSale(){
         return onSale;
     }
     
     @Override
-    public String toString()
-    {
+    public String toString(){
         return bikePart.toString() + "," + String.valueOf(onSale) + "," + String.valueOf(quantity);
     }
 
-    public String getName()
-    {
+    public String getName(){
         return bikePart.getName();
     }
 
-    public double getSalesPrice()
-    {
+    public double getSalesPrice(){
         return bikePart.getSalesPrice();
     }
 
-    public int getNumber()
-    {
+    public int getNumber(){
         return bikePart.getNumber();
     }
 
-    public double getListPrice()
-    {
+    public double getListPrice(){
         return bikePart.getListPrice();
     }
 }
