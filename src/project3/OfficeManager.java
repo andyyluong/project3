69f9d6df5f95555d9d6adc651bf3af91214d9963
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-10 14:26:39 -0500

    Added warehouse/office manager and system admin

---
+++ src/project3/OfficeManager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project3;
+
+/**
+ *
+ * @author andyluong
+ */
+public class OfficeManager extends LoginAccount {
+    
+    public OfficeManager(Person p, String aUserName, String aPassword) {
+        super(p, aUserName, aPassword);
+    }
+    
+}
---
+++ src/project3/SystemAdministrator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project3;
+
+/**
+ *
+ * @author andyluong
+ */
+public class SystemAdministrator extends LoginAccount {
+    
+        public SystemAdministrator(Person p, String aUserName, String aPassword) {
+            super(p, aUserName, aPassword);
+        }
+    
+}
