1cb397ab6a169fd6d9f2c1002cbd3b3eb8e2ab2e
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-10 14:10:28 -0500

    Updated javadoc comments

--- src/project3/LoginAccount.java
+++ src/project3/LoginAccount.java
@@ -1,43 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package project3;
 
+/**
+ * LoginAccount class contains the information of a login account
+ * @author andyluong
+ */
 public class LoginAccount {
-
-    public Person person;
+    
+    /**
+     * Data members
+     */
     public String userName;
     public String password;
-
-    public LoginAccount(Person person, String userName, String password) {
-        this.person = person;
-        this.userName = userName;
-        this.password = password;
+    public Person person;
+    
+    /**
+     * LoginAccount constructor
+     * @param aPerson Person's information
+     * @param aUserName Person's username
+     * @param aPassword Person's password
+     */
+    public LoginAccount(Person aPerson, String aUserName, String aPassword) {
+        person = aPerson;
+        userName = aUserName;
+        password = aPassword;
+        
     }
-
-    //-----------------------------------------------------------------------------------------------------------------
-    //Getters & Setters
-
+    
+    /**
+     * Method that sets a person
+     * @param aPerson Person 
+     */
+    public void setPerson(Person aPerson) {
+        person = aPerson;
+    }
+    
+    /**
+     * Method that gets a person
+     * @return A person
+     */
     public Person getPerson() {
         return person;
     }
-
-    public void setPerson(Person person) {
-        this.person = person;
+    
+    /**
+     * Method that sets a username for a person
+     * @param aUserName Username of a person
+     */
+    public void setUserName(String aUserName) {
+        userName = aUserName;
     }
-
+    
+    /**
+     * Method that gets a username
+     * @return Username of a person
+     */
     public String getUserName() {
         return userName;
     }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
+    
+    /**
+     * Method that sets a password
+     * @param aPassword password of a person
+     */
+    public void setPassword(String aPassword) {
+        password = aPassword;
     }
-
+    
+    /**
+     * Method that gets a password
+     * @return Password of a person
+     */
     public String getPassword() {
         return password;
     }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String toString() { return person + " " + userName + " " + password; }
+    
 }
