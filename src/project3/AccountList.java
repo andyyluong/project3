3a2f28ef3d7bcd26e877ba1b9de27f307c19cbb8
Brian-Justice <33060685+Brian-Justice@users.noreply.github.com>
2017-11-13 11:49:59 -0500

    Update AccountList.java

--- src/project3/AccountList.java
+++ src/project3/AccountList.java
@@ -1,22 +1,72 @@
-/**
-* @author andyluong
-* @author anthonyPhimmasone
-* @author brianJustice
-* @author alexLundin
-*/
 package project3;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+/**
+ *
+ * class Main
+ * @author andyluong
+ * @author anthonyPhimmasone
+ * @author brianJustice
+ * @author alexLudin
+ */
 
-public class AccountList implements Serializable{
-  private ArrayList<LoginAccount> accounts;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static WarehouseInventory warehouseInventory = new WarehouseInventory();
+    public static AccountList accountList = new AccountList();
   
-  public AccountList(){
-    accounts = new ArrayList<>();
-  }
-  
-  public void add(LoginAccount account){
-  accounts.add(account);
-  }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Bike Parts Distributorship Login");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+    
+  @Override
+  public void stop() throws FileNotFoundException, IOException{
+        FileOutputStream warehouseInventoryFileOut = new FileOutputStream("warehouseInvetory.ser");
+        FileOutputStream accountListFileOut = new FileOutputStream("accountList.ser");
+        ObjectOutputStream warehouseInventoryOut = new ObjectOutputStream(warehouseInventoryFileOut);
+        ObjectOutputStream accountListOut = new ObjectOutputStream(accountListFileOut);
+        warehouseInventoryOut.writeObject(warehouseInventory);
+        warehouseInventoryOut.close();
+        warehouseInventoryFileOut.close();
+        accountListOut.writeObject(accountList);
+        accountListOut.close();
+        accountListFileOut.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        File file1 = new File("warehouseInventory.ser");
+        File file2 = new File("accountList.ser");
+        if(file1.exists())
+        { 
+            FileInputStream warehouseInventoryFile = new FileInputStream("warehouseInventory.ser");            
+            ObjectInputStream warehouseInventoryIn = new ObjectInputStream(warehouseInventoryFile);            
+            warehouseInventory = (WarehouseInventory) warehouseInventoryIn.readObject();
+            warehouseInventoryIn.close();
+            warehouseInventoryFile.close();            
+        }
+        if(file2.exists())
+        {
+            FileInputStream accountListFile = new FileInputStream("accountList.ser");
+            ObjectInputStream accountListIn  = new ObjectInputStream(accountListFile);
+            accountList = (AccountList) accountListIn.readObject();
+            accountListIn.close();
+            accountListFile.close();                                                 
+        }
+            launch(args);
+    }
 }
