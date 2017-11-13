0ead779fd257e56595156744f5e691bf25a40260
Brian-Justice <33060685+Brian-Justice@users.noreply.github.com>
2017-11-13 10:34:09 -0500

    Create WarehouseInventory.java

---
+++ src/project3/WarehouseInventory.java
@@ -0,0 +1,21 @@
+package project3
+
+/**
+* @author andyluong
+* @author anthonyPhimmasone
+* @author brianJustice
+* @author alexLundin
+*/
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class WarehouseInventory{
+  private ArrayList<Warehouse> warehouseInventory;
+  
+  public WarehouseInventory(){
+    warehouseInventory = new ArrayList<>();
+    }
+  
+  public void add(Warehouse warehouse){
+    warehouseInventory.add(warehouse);
+  }
