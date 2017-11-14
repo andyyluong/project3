931c9428e9c1ec40113898783ddf27015819af73
Brian-Justice <33060685+Brian-Justice@users.noreply.github.com>
2017-11-14 12:57:55 -0500

    Create AccountType.java

---
+++ src/project3/AccountType.java
@@ -0,0 +1,11 @@
+package project3;
+/**
+* @author brianJustice
+* @author andyluong
+* @author anthonyPhimmasone
+* @author alexLundin
+*/
+
+public enum AccountType{
+  SALES_ASSOCIATE, WAREHOUSE_MANAGER, ADMIN, OFFICE_MANAGER
+  }
