4d14493fd3284bec203620517830975d5c12b214
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-14 09:41:16 -0500

    Update Person.java javadoc comments

--- src/project3/Person.java
+++ src/project3/Person.java
@@ -1,5 +1,7 @@
 package project3;
+
 import java.io.Serializable;
+
 /**
  *
  * Person class defines a person 
@@ -8,15 +10,25 @@ import java.io.Serializable;
  * @author brianJustice
  *
  */
-
-public class Person implements Serializable{
-
+public class Person implements Serializable {
+    
+    /**
+     * Data members
+     */
     public String firstName;
     public String lastName;
     public int age;
     public int phoneNumber;
     public String emailAddress;
 
+    /**
+     * Person constructor
+     * @param firstName First name of a person
+     * @param lastName Last name of a person
+     * @param age Age of a person
+     * @param phoneNumber Phone number of a person
+     * @param aEmailAddress Email address of a person
+     */
     public Person(String firstName, String lastName, int age, int phoneNumber,
            String aEmailAddress) {
         this.firstName = firstName;
@@ -25,37 +37,70 @@ public class Person implements Serializable{
         this.phoneNumber = phoneNumber;
         this.emailAddress = aEmailAddress;
     }
+     
     //-----------------------------------------------------------------------------------------------------------------
     //Getters & Setters
 
+    /**
+     * Get first name of a person
+     * @return First name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Set first name of a person
+     * @param firstName First name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Get last name of a person
+     * @return Last name of a person
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Set last name of a person
+     * @param lastName Last name of a person
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Get the age of a person
+     * @return Age of a person
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Set the age of a person
+     * @param age Age of a person
+     */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * Get the phone number of a person
+     * @return Phone number
+     */
     public int getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Set the phone number of a person
+     * @param phoneNumber Phone number of a person
+     */
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
@@ -64,18 +109,20 @@ public class Person implements Serializable{
         return emailAddress;
     }
 
+    /**
+     * Set the email address of a person
+     * @param emailAddress Email address of a person
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
     /**
-     * method toString
-     * @return firstName
-     * @return lastName
-     * @return age
-     * @return phoneNumber
-     * @return emailAddress
-     *
+     * toString Displays person attributes in a string
+     * @return First name, last name, age, phone number, and email address
      */
-    public String toString() {return firstName + " " + lastName + " " + age + " " + phoneNumber + " " + emailAddress;}
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " " + age + " " + phoneNumber + " " + emailAddress;
+    }
 }
