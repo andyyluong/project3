22225293300d079a65a8e699cbe39219e5b3e889
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-14 09:50:44 -0500

    Update AccountList.java javadoc comments

--- src/project3/AccountList.java
+++ src/project3/AccountList.java
@@ -1,22 +1,31 @@
-/**
-* @author andyluong
-* @author anthonyPhimmasone
-* @author brianJustice
-* @author alexLundin
-*/
 package project3;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+* AccountList class that contains all the login accounts information
+* @author andyluong
+* @author anthonyPhimmasone
+* @author brianJustice
+* @author alexLundin
+*/
 public class AccountList implements Serializable{
   private ArrayList<LoginAccount> accounts;
   
+  /**
+   * AccountList constructor
+   */
   public AccountList(){
     accounts = new ArrayList<>();
   }
   
+  /**
+   * Add a login account
+   * @param account Login account
+   */
   public void add(LoginAccount account){
-  accounts.add(account);
+    accounts.add(account);
   }
+  
 }
