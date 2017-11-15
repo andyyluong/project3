69b1dd783a5883b136265e8617e367f153010dec
Brian-Justice <33060685+Brian-Justice@users.noreply.github.com>
2017-11-15 09:24:38 -0500

    Update WarehouseManager.java

--- src/project3/WarehouseManager.java
+++ src/project3/WarehouseManager.java
@@ -10,9 +10,17 @@ import java.io.Serializable;
  * @author andyluong
  */
 public class WarehouseManager extends LoginAccount implements Serializable {
+
+    private AccountType accountType;
     
     public WarehouseManager(Person p, String aUserName, String aPassword) {
         super(p, aUserName, aPassword);
+        accountType = AccountType.WAREHOUSE_MANAGER;
+    }
+
+    @Override
+    public AccountType getType() {
+        return accountType;
     }
     
 }
