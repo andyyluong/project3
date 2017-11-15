15cffb6af43569ccb666df6aa1ca319fb47255df
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:03:56 -0500

    Add files via upload

--- src/project3/Part.java
+++ src/project3/Part.java
@@ -1,36 +1,64 @@
-package project3;
-
-/**
- *
- * class Part
- * @author andyluong
- * @author anthonyPhimmasone
- * @author brianJustice
- *
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
+/**
+ * Part class contains information about a part
+ * @author andyLuong
+ * @author anthonyPhimmasone
+ * @author brianJustice
+ * @author alexLundin
+ */
 public class Part {
     protected String name;
     protected String number;
     protected Cost price;
 
-
+    /**
+     * Part constructor
+     * @param name Name of part
+     * @param number Number of part
+     * @param cost Cost of part
+     */
     public Part(String name, String number, String cost) {
         this.name = name;
         this.number = number;
         this.price = new Cost(cost);
     }
 
-    public String getName() {
-        return name;
+    /**
+     * Get name of part
+     * @return Name of part
+     */
+    public String getName() { 
+        return name; 
     }
-    public String getNumber() {
-        return number;
+    
+    /**
+     * Get number of part
+     * @return Number of part
+     */
+    public String getNumber() { 
+        return number; 
     }
-    public String getPrice() {
-        return price.toString();
+    
+    /**
+     * Get price of part
+     * @return Price of part
+     */
+    public String getPrice() { 
+        return price.toString(); 
     }
-    public void setPrice(String price) {
-        this.price = new Cost(price);
+    
+    /**
+     * Set price of part
+     * @param price Price
+     */
+    public void setPrice(String price) { 
+        this.price = new Cost(price); 
     }
+
+    
 }
