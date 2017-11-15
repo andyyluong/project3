31aa0c9394e71aeb2113ba92363211dc67240b7c
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 13:43:37 -0500

    Update Cost.java javadoc comments

--- src/project3/Cost.java
+++ src/project3/Cost.java
@@ -1,37 +1,67 @@
-package project3;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * class Cost
- * @author andyluong
+ * Cost uses BigDecimal to store $ values with 2 decimal digits.
+ * @author andyLuong
  * @author anthonyPhimmasone
  * @author brianJustice
- * Cost uses BigDecimal to store $ values with 2 decimal digits.
- *
+ * @author alexLundin
  */
-
 public class Cost {
-
+    
     private BigDecimal cost;
 
-    public Cost(String cost) {
+    /**
+     * Cost constructor
+     * @param cost Cost value
+     */
+    public Cost(String cost) { 
         this.cost = new BigDecimal(cost);
     }
 
-    public String getCost() { return cost.setScale(2, RoundingMode.HALF_EVEN).toString(); }
-
-    public void setCost(String cost) {
-        this.cost = new BigDecimal(cost);
-    }
-
-    public String toString(){
+    /**
+     * Get cost value
+     * @return Cost value
+     */
+    public String getCost() { 
         return cost.setScale(2, RoundingMode.HALF_EVEN).toString();
     }
 
-    public boolean equals(Cost c) {
-        return cost.setScale(2, RoundingMode.HALF_EVEN).toString().equals(c.getCost());
+    /**
+     * Set cost value
+     * @param cost Cost value
+     */
+    public void setCost(String cost) { 
+        this.cost = new BigDecimal(cost); 
     }
 
+    /**
+     * Display cost value
+     * @return Cost value
+     */
+    @Override
+    public String toString(){ 
+        return cost.setScale(2, RoundingMode.HALF_EVEN).toString(); 
+    }
+
+    /**
+     * Boolean method to determine cost value
+     * @param c Cost
+     * @return Cost if it equals cost value
+     */
+    public boolean equals(Cost c) {
+      return cost.setScale(2, RoundingMode.HALF_EVEN).toString().equals(c.getCost());
+    }
+
+    /**
+     * Compare two cost values
+     * @param c Cost
+     * @return Comparison of two cost values
+     */
+    int compareTo(Cost c) {
+        return this.getCost().compareTo(c.getCost());
+    }
+    
 }
+
