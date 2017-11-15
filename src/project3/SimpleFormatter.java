f2bb7ac66c533a803c5d898d16ee0bb9b5447736
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:13:07 -0500

    Updated SimpleFormatter.java javadoc comments

--- src/project3/SimpleFormatter.java
+++ src/project3/SimpleFormatter.java
@@ -13,18 +13,31 @@ package project3;
      
     private double total;
      
+    /**
+     * Format header
+     * @return Invoice header
+     */
     @Override
     public String formatHeader() {
         total = 0;
         return " I N V O I C E\n\n\n";
     }
 
+    /**
+     * Format sales item
+     * @param item Sales item
+     * @return Sales item and price
+     */
     @Override
     public String formatSalesItem(SalesItem item) {
         total += item.getPrice();
         return (String.format("%s: $%.2f\n", item.toString(), item.getPrice()));
     }
 
+    /**
+     * Format footer
+     * @return Invoice footer
+     */
     @Override
     public String formatFooter() {
         return (String.format("\n\nTOTAL DUE: $%.2f\n", total));
