1ece967073cb55cc459e9e80220c174f960be5ed
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:27:45 -0500

    Updated WarehouseInventory.java javadoc comments

--- src/project3/WarehouseInventory.java
+++ src/project3/WarehouseInventory.java
@@ -9,14 +9,22 @@ package project3;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class WarehouseInventory{
+public class WarehouseInventory implements Serializable {
   private ArrayList<Warehouse> warehouseInventory;
   
+  /**
+   * Warehouse inventory constructor
+   */
   public WarehouseInventory(){
     warehouseInventory = new ArrayList<>();
     }
   
+  /**
+   * Add a warehouse to warehouse inventory
+   * @param warehouse Warehouse being added
+   */
   public void add(Warehouse warehouse){
     warehouseInventory.add(warehouse);
   }
+  
 }
