d58bde1042ee03cd85e17c0d9bff0fabd8ee68f5
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:30:48 -0500

    Updated WarehouseManager.java javadoc comments

--- src/project3/WarehouseManager.java
+++ src/project3/WarehouseManager.java
@@ -1,23 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3;
+
 import java.io.Serializable;
 /**
- *
+ * Warehouse Manager class contains actions for a warehouse manager employee
  * @author andyluong
  */
 public class WarehouseManager extends LoginAccount implements Serializable {
 
     private AccountType accountType;
     
+    /**
+     * Warehouse Manager constructor
+     * @param p Person that is a warehouse manager
+     * @param aUserName Username of a warehouse manager
+     * @param aPassword Password of a warehouse manager
+     */
     public WarehouseManager(Person p, String aUserName, String aPassword) {
         super(p, aUserName, aPassword);
         accountType = AccountType.WAREHOUSE_MANAGER;
     }
 
+    /**
+     * Get account type
+     * @return Account type
+     */
     @Override
     public AccountType getType() {
         return accountType;
