73a543f2a1d8d88a2984b5c646fb70a2d9fecc0e
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:18:10 -0500

    Updated Subject.java javadoc comments

--- src/project3/Subject.java
+++ src/project3/Subject.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Subject class contains the subject for the observer
  * @author andyluong
  */
 public class Subject {
