8a57296f1659566242a905e317bcc5120ffc89ea
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 13:46:31 -0500

    Updated DiscountedItem.java javadoc comments

--- src/project3/DiscountedItem.java
+++ src/project3/DiscountedItem.java
@@ -9,29 +9,43 @@ package project3;
  * A decorator for an item that applies a discount
  * @author andyluong
  */
-
 public class DiscountedItem implements SalesItem {
 
     private final double discount;
     private final SalesItem item;
     
+    /**
+     * Discounted Item constructor
+     * @param item Bike part item
+     * @param discount Discount of bike part item
+     */
     public DiscountedItem(SalesItem item, double discount) {
         this.item = item;
         this.discount = discount;
     }
 
-
+    /**
+     * Get price of discounted item
+     * @return Discounted item
+     */
     @Override
     public double getPrice() {
         return item.getPrice() * (1 - discount / 100);
     }
     
-
+    /**
+     * Display discounted item
+     * @return Discounted item
+     */
     @Override
     public String toString() {
         return item.toString() + " (Discount " + discount + "%)";
     }
     
+    /**
+     * Get name of discounted item
+     * @return Discounted bike part item name
+     */
     @Override
     public String getName() {
         return item.getName();
