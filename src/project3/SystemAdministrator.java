4e7b824afb0eff09c52acf30ce0f89a561960324
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:21:50 -0500

    Updated SystemAdministrator.java javadoc comments

--- src/project3/SystemAdministrator.java
+++ src/project3/SystemAdministrator.java
@@ -1,23 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3;
 
 /**
- *
+ * System Administrator class contains actions for system administrator employee
  * @author andyluong
  */
 public class SystemAdministrator extends LoginAccount {
 
     private AccountType accountType;
     
+    /**
+     * System Administrator constructor
+     * @param p Person that is a system administrator
+     * @param aUserName Username of system administrator
+     * @param aPassword Password of system administrator
+     */
     public SystemAdministrator(Person p, String aUserName, String aPassword) {
         super(p, aUserName, aPassword);
         accountType = AccountType.ADMIN;
     }
 
+    /**
+     * Get account type
+     * @return System administrator account type
+     */
     @Override
     public AccountType getType() {
         return accountType;
