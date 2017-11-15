3ea2211765e9e757c677563e57c08d4b2231118d
Anthony-Phimmasone <31628907+Anthony-Phimmasone@users.noreply.github.com>
2017-11-15 13:20:39 -0500

    Added  a delete account method
    Added  a delete account method

--- src/project3/AccountList.java
+++ src/project3/AccountList.java
@@ -4,42 +4,58 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
-* AccountList class that contains all the login accounts information
-* @author andyluong
-* @author anthonyPhimmasone
-* @author brianJustice
-* @author alexLundin
-*/
+ * AccountList class that contains all the login accounts information
+ * @author andyluong
+ * @author anthonyPhimmasone
+ * @author brianJustice
+ * @author alexLundin
+ */
 public class AccountList implements Serializable{
-  private ArrayList<LoginAccount> accounts;
-  private ArrayList<SalesAssociate> salesAssociates;
-  private OfficeManager officeManager;
-  private SystemAdministrator systemAdministrator;
-  private WarehouseManager warehouseManager;
-  /**
-   * AccountList constructor
-   */
-  public AccountList(){
-    accounts = new ArrayList<>();
-  }
-  
-  /**
-   * Add a login account
-   * @param account Login account
-   */
-  public void add(LoginAccount account){
-    if(account.getType()==AccountType.SALES_ASSOCIATE){
-      salesAssociates.add((SalesAssociate) account);
+    private ArrayList<LoginAccount> accounts;
+    private ArrayList<SalesAssociate> salesAssociates;
+    private OfficeManager officeManager;
+    private SystemAdministrator systemAdministrator;
+    private WarehouseManager warehouseManager;
+    /**
+     * AccountList constructor
+     */
+    public AccountList(){
+        accounts = new ArrayList<>();
     }
-    if(account.getType()==AccountType.OFFICE_MANAGER){
-        officeManager = (OfficeManager) account;
+
+    /**
+     * Add a login account
+     * @param account Login account
+     */
+    public void add(LoginAccount account){
+        if(account.getType()==AccountType.SALES_ASSOCIATE){
+            salesAssociates.add((SalesAssociate) account);
+        }
+        if(account.getType()==AccountType.OFFICE_MANAGER){
+            officeManager = (OfficeManager) account;
+        }
+        if(account.getType()==AccountType.ADMIN){
+            systemAdministrator = (SystemAdministrator) account;
+        }
+        if(account.getType()==AccountType.WAREHOUSE_MANAGER){
+            warehouseManager = (WarehouseManager) account;
+        }
+        accounts.add(account);
     }
-    if(account.getType()==AccountType.ADMIN){
-        systemAdministrator = (SystemAdministrator) account;
-    }
-    if(account.getType()==AccountType.WAREHOUSE_MANAGER){
-        warehouseManager = (WarehouseManager) account;
-    }
-    accounts.add(account);
+
+    public void delete(LoginAccount account){
+        if(account.getType()==AccountType.SALES_ASSOCIATE){
+            salesAssociates.remove((SalesAssociate) account);
+        }
+        if(account.getType()==AccountType.OFFICE_MANAGER){
+            officeManager = (OfficeManager) account;
+        }
+        if(account.getType()==AccountType.ADMIN){
+            systemAdministrator = (SystemAdministrator) account;
+        }
+        if(account.getType()==AccountType.WAREHOUSE_MANAGER){
+            warehouseManager = (WarehouseManager) account;
+        }
+        accounts.remove(account);
     }
 }
