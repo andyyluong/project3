40f129b66ca5d23c5d21df9e6075cff7f0b33cc7
Brian-Justice <33060685+Brian-Justice@users.noreply.github.com>
2017-11-15 09:25:45 -0500

    Create MainWarehouse.java

---
+++ src/project3/MainWarehouse.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project3;
+
+/**
+ *
+ * @author brian
+ */
+public class MainWarehouse extends Warehouse{
+    
+    public MainWarehouse(String name, WarehouseType warehouseType) {
+        super(name, warehouseType);
+    }
+
+    @Override
+    public Inventory findInventoryByName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Inventory findInventoryByNumber(int number) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addInventory(String part) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void sell(int partNumber, int amount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
