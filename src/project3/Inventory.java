3f9b1cc520f6a8bbfdfc74b99f4ec17b57e9dfcc
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:51:35 -0500

    Update Inventory.java javadoc comments

--- src/project3/Inventory.java
+++ src/project3/Inventory.java
@@ -5,53 +5,96 @@ import java.io.Serializable;
 
 /**
  * Inventory class defines an inventory
- * @author andyLuong
+ * @author andyluong
  * @author anthonyPhimmasone
  * @author brianJustice
  * @author alexLundin
  */
 public class Inventory implements Serializable {
+    
     private BikePart bikePart;
     private Boolean onSale;
     private int quantity;
     
+    /**
+     * Inventory constructor
+     * @param bikepart Bike part
+     * @param onSale On sale or not
+     */
     public Inventory(BikePart bikepart, Boolean onSale){
         this.bikePart = bikepart;
         this.onSale = onSale;
     }
+    
+    /**
+     * Add quantity of inventory
+     * @param number Number of quantity added
+     */
     public void add(int number){
         quantity += number;
     }
     
+    /**
+     * Take quantity of inventory
+     * @param number Number of quantity taken
+     */
     public void take(int number){
         quantity -= number;
     }
     
+    /**
+     * Set bike part on sale
+     * @param onSale Bike part on sale or not
+     */
     public void setOnSale(boolean onSale){
         this.onSale = onSale;
     }
     
+    /**
+     * Get on sale
+     * @return Bike part on sale or not
+     */
     public Boolean getOnSale(){
         return onSale;
     }
     
+    /**
+     * Display bike part attributes
+     * @return Bike part name, number, price, sales price, on sale, and quantity
+     */
     @Override
     public String toString(){
         return bikePart.toString() + "," + onSale + "," + quantity;
     }
 
+    /**
+     * Get name of bike part
+     * @return Bike part name
+     */
     public String getName(){
         return bikePart.getPartName();
     }
 
+    /**
+     * Get sales price of bike part
+     * @return Bike part sales price
+     */
     public double getSalesPrice(){
         return bikePart.getSalesPrice();
     }
 
+    /**
+     * Get part number of bike part
+     * @return Bike part number
+     */
     public int getNumber(){
         return bikePart.getPartNumber();
     }
 
+    /**
+     * Get list price of bike part
+     * @return Bike part list price
+     */
     public double getListPrice(){
         return bikePart.getListPrice();
     }
