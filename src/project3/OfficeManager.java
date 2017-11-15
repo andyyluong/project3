891dad861896cdeb69cdc42a5fa48e88b330fc5a
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-15 14:00:13 -0500

    Updated OfficeManager.java javadoc comments

--- src/project3/OfficeManager.java
+++ src/project3/OfficeManager.java
@@ -1,23 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3;
 
 /**
- *
+ * Office Manager class contains actions for office manager employee
  * @author andyluong
  */
 public class OfficeManager extends LoginAccount {
 
     private AccountType accountType;
     
+    /**
+     * 
+     * @param p Person that is a office manager
+     * @param aUserName Username of office manager
+     * @param aPassword Password of office manager
+     */
     public OfficeManager(Person p, String aUserName, String aPassword) {
         super(p, aUserName, aPassword);
         accountType = AccountType.OFFICE_MANAGER;
     }
 
+    /**
+     * Get account type
+     * @return Account type
+     */
     @Override
     public AccountType getType() {
         return accountType;
