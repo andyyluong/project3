4dc7ab7cb3eeb27cf931b13d7144d52780f03d0c
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-20 10:52:39 -0500

    Update Cost.java to cost package

---
+++ src/Cost.java
@@ -0,0 +1,69 @@
+package cost; 
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * Cost uses BigDecimal to store $ values with 2 decimal digits.
+ * @author andyLuong
+ * @author anthonyPhimmasone
+ * @author brianJustice
+ * @author alexLundin
+ */
+public class Cost {
+    
+    private BigDecimal cost;
+
+    /**
+     * Cost constructor
+     * @param cost Cost value
+     */
+    public Cost(String cost) { 
+        this.cost = new BigDecimal(cost);
+    }
+
+    /**
+     * Get cost value
+     * @return Cost value
+     */
+    public String getCost() { 
+        return cost.setScale(2, RoundingMode.HALF_EVEN).toString();
+    }
+
+    /**
+     * Set cost value
+     * @param cost Cost value
+     */
+    public void setCost(String cost) { 
+        this.cost = new BigDecimal(cost); 
+    }
+
+    /**
+     * Display cost value
+     * @return Cost value
+     */
+    @Override
+    public String toString(){ 
+        return cost.setScale(2, RoundingMode.HALF_EVEN).toString(); 
+    }
+
+    /**
+     * Boolean method to determine cost value
+     * @param c Cost
+     * @return Cost if it equals cost value
+     */
+    public boolean equals(Cost c) {
+      return cost.setScale(2, RoundingMode.HALF_EVEN).toString().equals(c.getCost());
+    }
+
+    /**
+     * Compare two cost values
+     * @param c Cost
+     * @return Comparison of two cost values
+     */
+    int compareTo(Cost c) {
+        return this.getCost().compareTo(c.getCost());
+    }
+    
+}
+
