c7c139bb9316506403f3cb14ca02c7c9d2b0da6c
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-21 09:49:19 -0500

    Update Warehouse.java

--- src/project3/Warehouse.java
+++ src/project3/Warehouse.java
@@ -1,7 +1,12 @@
 package project3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Warehouse class contains information about a warehouse
  * @author brianJustice
@@ -27,6 +32,7 @@ public abstract class Warehouse implements Serializable
         this.warehouseType = warehouseType;
     }
     
+    
     /**
      * Get warehouse type
      * @return Warehouse type
@@ -35,14 +41,44 @@ public abstract class Warehouse implements Serializable
     {
         return warehouseType;
     }
+    
+    public String read(String filename) {
+       ArrayList<Inventory> invList = new ArrayList<>();
+       String retString = new String();
+       File input = new File(filename);
+        try {
+            Scanner read = new Scanner(input);
+            while(read.hasNextLine()) {
+                String line = read.nextLine();
+                String regularExpression = "\\s*(\\s|,)\\s*";
+                String[] bpWH = line.split(regularExpression);
+                System.out.println(Arrays.toString(bpWH));
+                Inventory inv = new Inventory(new BikePart(bpWH[0], Integer.parseInt(bpWH[1]), Double.parseDouble(bpWH[2]), Double.parseDouble(bpWH[3])), Boolean.parseBoolean(bpWH[4]), Integer.parseInt(bpWH[5]));
+                invList.add(inv);
+                retString += inv.toString() + "\n";
+                
+            }
             
-    public abstract Inventory findInventoryByName(String name);
+            return retString;
+        }
+
+        catch(FileNotFoundException e) {
+            System.out.println("File Not Found");
+            e.printStackTrace();
+        }
+        return "File Not Found 2";        
+        
+    }
+            
+    //public Inventory findInventoryByName(String name);
     
-    public abstract Inventory findInventoryByNumber(int number);
+    //public Inventory findInventoryByNumber(int number);
     
-    public abstract void addInventory(String part);
+    //public void addInventory(String part);
     
-    public abstract String sortName();
+    //public void sell(int partNumber, int amount);
     
-    public abstract String sortNumber();
+    //public String sortName();
+    
+    //public String sortNumber();
 }
