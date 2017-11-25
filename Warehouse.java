cd1bf6371f88bfc4c97baa3c4577bf1333a6a077
Anthony-Phimmasone <31628907+Anthony-Phimmasone@users.noreply.github.com>
2017-11-24 19:13:40 -0500

    Updated Examine Parts
    Updated Examine Parts

---
+++ Warehouse.java
@@ -0,0 +1,111 @@
+package project3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Warehouse class contains information about a warehouse
+ * @author brianJustice
+ * @author andyLuong
+ * @author anthonyPhimmasone
+ * @author alexLundin
+ */
+public abstract class Warehouse implements Serializable
+{
+    private ArrayList<Inventory> inventoryWarehouse;
+    private String name;
+    private WarehouseType warehouseType;
+
+    /**
+     * Warehouse constructor
+     * @param name Name of warehouse
+     * @param warehouseType Warehouse type
+     */
+    public Warehouse(String name, WarehouseType warehouseType)
+    {
+        inventoryWarehouse = new ArrayList<>();
+        this.name = name;
+        this.warehouseType = warehouseType;
+    }
+
+
+    /**
+     * Get warehouse type
+     * @return Warehouse type
+     */
+    public WarehouseType getType()
+    {
+        return warehouseType;
+    }
+
+    public String read(String filename) {
+        ArrayList<Inventory> invList = new ArrayList<>();
+        String retString = new String();
+        File input = new File(filename);
+        try {
+            Scanner read = new Scanner(input);
+            while(read.hasNextLine()) {
+                String line = read.nextLine();
+                String regularExpression = "\\s*(\\s|,)\\s*";
+                String[] bpWH = line.split(regularExpression);
+                System.out.println(Arrays.toString(bpWH));
+                Inventory inv = new Inventory(new BikePart(bpWH[0], Integer.parseInt(bpWH[1]), Double.parseDouble(bpWH[2]), Double.parseDouble(bpWH[3])), Boolean.parseBoolean(bpWH[4]), Integer.parseInt(bpWH[5]));
+                invList.add(inv);
+                retString += inv.toString() + "\n";
+
+            }
+
+            return retString;
+        }
+
+        catch(FileNotFoundException e) {
+            System.out.println("File Not Found");
+            e.printStackTrace();
+        }
+        return "File Not Found 2";
+
+    }
+
+    public static String doDisplay(String displayBikePart) {
+
+        String partName = displayBikePart;
+
+        boolean Found = false;
+        int j = 0;
+
+        while (j < Inventory.toString().size() && !Found) {
+
+            if (Inventory.toString().get(j).getPartName().equals(partName)) {
+                Found = true;
+                if (Inventory.toString().get(j).getOnSale()) {
+                    //Part Name is on sale this is the sales price
+                    return (Inventory.toString().get(j).getPartName() + " " + Inventory.toString().get(j).getSalesPrice());
+                } else if (!Inventory.toString().get(j).getOnSale()) {
+                    //Part Name is not on sale this is the list price
+                    return(Inventory.toString().get(j).getPartName() + " " + Inventory.toString().get(j).getListPrice());
+                }
+            }
+            j += 1;
+        }
+
+        if (!Found) {
+        }
+        return("Part Name Not Found!");
+    }
+
+    //public Inventory findInventoryByName(String name);
+
+    //public Inventory findInventoryByNumber(int number);
+
+    //public void addInventory(String part);
+
+    //public void sell(int partNumber, int amount);
+
+    //public String sortName();
+
+    //public String sortNumber();
+}
