71c36306c0a63151111da92fcdc23a49eec6c390
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-11-27 08:34:24 -0500

    Update Controller.java
    Place into correct directory

--- src/project3/Controller.java
+++ src/project3/Controller.java
@@ -188,7 +188,14 @@ public class Controller {
         String partname = partNameOfficeManager.getText();
         String partnumber = partNumberOfficeManager.getText();
 
+        myTextAreaOfficeText.appendText("Part Name " + " Part Price" + "\n");
+        myTextAreaOfficeText.appendText(partname);
+        myTextAreaOfficeText.appendText(partnumber);
+        myTextAreaOfficeText.appendText(Main.mainWH(param) + "\n");
+        if (bikePartArrayList != null) {
 
+        } else
+            myTextAreaOfficeText.appendText("Part Name Not Found");
     }
 
 
@@ -222,7 +229,7 @@ public class Controller {
                         officeManagerTab.setDisable(true);
                         salesAssociateTab.setDisable(true);
                         selectionModel.select(systemAdminTab);
-                        break;
+                    break;
                     case OFFICE_MANAGER:
                         systemAdminTab.setDisable(true);
                         loginscreenTab.setDisable(true);
@@ -230,7 +237,7 @@ public class Controller {
                         officeManagerTab.setDisable(false);
                         salesAssociateTab.setDisable(true);
                         selectionModel.select(officeManagerTab);
-                        break;
+                    break;
                     case WAREHOUSE_MANAGER:
                         systemAdminTab.setDisable(true);
                         loginscreenTab.setDisable(true);
@@ -238,7 +245,7 @@ public class Controller {
                         officeManagerTab.setDisable(true);
                         salesAssociateTab.setDisable(true);
                         selectionModel.select(warehouseManagerTab);
-                        break;
+                    break;
                     case SALES_ASSOCIATE:
                         systemAdminTab.setDisable(true);
                         loginscreenTab.setDisable(true);
@@ -246,9 +253,9 @@ public class Controller {
                         officeManagerTab.setDisable(true);
                         salesAssociateTab.setDisable(false);
                         selectionModel.select(salesAssociateTab);
-                        break;
+                    break;
                     default:
-                        break;
+                    break;
                 }
             }
         }
@@ -308,6 +315,15 @@ public class Controller {
 
     @FXML
     void doGenerateSalesInvoice(ActionEvent event) {
+        String customer = CustomerNameAssociateText.getText();
+        if(customer.equals("")) {
+            myTextAreaAssociateText.appendText("Must enter customer name");
+        }
+        SalesInvoice salesInvoice = new SalesInvoice(new Date(), customer);
+        myTextAreaAssociateText.appendText("Sales invoice for: " + customer + "\n");
+        myTextAreaAssociateText.appendText("                        List   Sale          \n");
+        myTextAreaAssociateText.appendText("Part Name  Part Number  Price  Price  Quantity   Total\n");
+        double aTotalInvoicePrice = 0;
 
     }
 
@@ -332,7 +348,17 @@ public class Controller {
 
     @FXML
     void doGenerateSalesPaycheck(ActionEvent event) {
-
+        Date start, end;
+        String startDate = StartDateText.getText();
+        String endDate = EndDateText.getText();
+        if (startDate.equals("") && endDate.equals("")) {
+            start = null;
+            end = null;
+        }
+        else {
+            start = new Date();
+            end = new Date();
+        }
     }
 
     @FXML
