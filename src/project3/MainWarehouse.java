e1ce43854649d54f6416755c8a1da6e67b679101
Andy Luong <31705702+andyyluong@users.noreply.github.com>
2017-12-02 09:35:41 -0500

    Updated MainWarehouse.java
    Removed unsupported methods

--- src/project3/MainWarehouse.java
+++ src/project3/MainWarehouse.java
@@ -19,24 +19,6 @@ public class MainWarehouse extends Warehouse implements Serializable{
     public MainWarehouse(String name, WarehouseType warehouseType) {
         super(name, warehouseType);
     }
-    
-    /**
-     * Find inventory by name
-     * @param name Name of bike part in inventory
-     * @return Bike part by name
-     */
-    public Inventory findInventoryByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    /**
-     * Find inventory by number
-     * @param number Number of bike part in inventory
-     * @return Bike part by number
-     */
-    public Inventory findInventoryByNumber(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     /**
      * Add inventory
@@ -45,15 +27,6 @@ public class MainWarehouse extends Warehouse implements Serializable{
     public void addInventory(String part) {
         String qualities[] = part.split(",");
         inventoryWarehouse.add(new Inventory(new BikePart(qualities[0], Integer.parseInt(qualities[1]), Double.parseDouble(qualities[2]), Double.parseDouble(qualities[3])), Boolean.parseBoolean(qualities[4]), Integer.parseInt(qualities[5])));
-    }
-
-    /**
-     * Sell inventory
-     * @param partNumber Part number of bike part to be sold
-     * @param amount Amount to be sold
-     */
-    public void sell(int partNumber, int amount) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
